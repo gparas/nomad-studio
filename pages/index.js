85f4971339fb16039f1da74deafe1fd70f9a5f67
georgios.parapanissios <georgios.parapanissios@marinetraffic.com>
2021-11-22 12:19:59 +0200

    stagger elements with framer motion

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,57 @@
 import { motion } from 'framer-motion';
 
-const Home = () => {
-  const container = {
-    hidden: { y: 64 },
-    show: {
-      y: 0,
-      transition: {
-        staggerChildren: 0.2,
-      },
+const variants = {
+  initial: { y: 400 },
+  animate: (i) => ({
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1,
+      delay: i * 0.2,
     },
-  };
+  }),
+};
 
-  const item = {
-    hidden: { y: 64 },
-    show: { y: 10 },
-  };
+const Title = ({ item, custom }) => {
+  const { title, className } = item;
+  return (
+    <div className="h-20 flex justify-end overflow-hidden">
+      <motion.div
+        className={className}
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        custom={custom}
+      >
+        {title}
+      </motion.div>
+    </div>
+  );
+};
+
+const titles = [
+  {
+    title: 'ELEGANT',
+    className: 'text-7xl font-bold text-outline leading-tight',
+  },
+  {
+    title: 'DIFFERENTIATING',
+    className: 'text-7xl font-serif',
+  },
+  {
+    title: 'DESIGN',
+    className: 'text-7xl font-bold leading-tight',
+  },
+];
+
+const Home = () => {
   return (
     <section className="h-screen flex items-center justify-center">
-      <motion.h2 variants={container} initial="hidden" animate="show">
-        <div className="h-20 flex justify-end overflow-hidden">
-          <motion.div
-            variants={item}
-            className="text-7xl font-bold text-outline"
-          >
-            ELEGANT
-          </motion.div>
-        </div>
-        <div className="h-20 flex justify-end overflow-hidden">
-          <motion.div variants={item} className="text-7xl font-bold font-serif">
-            DIFFERENTIATING
-          </motion.div>
-        </div>
-        <div className="h-20 flex justify-end overflow-hidden">
-          <motion.div variants={item} className="text-7xl font-bold">
-            DESIGN
-          </motion.div>
-        </div>
-      </motion.h2>
+      <h2>
+        {titles.map((item, i) => (
+          <Title custom={i} key={item} item={item} />
+        ))}
+      </h2>
     </section>
   );
 };
